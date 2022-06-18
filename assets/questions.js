bb62c765cf9d3f7beae92579240b767804c09198
rockwoodc <rockwoodce@gmail.com>
2022-06-18 12:11:58 -0400

    hide welcome and display answers

---
+++ assets/questions.js
@@ -0,0 +1,7 @@
+var questions = [
+    {
+        message:"If you want to make a message appear on the screen in a seperate box, which code would you need?",
+        choices: ["window.alert()", "window.prompt()", "stringDataType", "An if/else statement"],
+        correctAnswer:"window.alert()"
+    }
+]
--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,21 @@
+var questionIndex = 0
 var timerEl = document.getElementById("timer");
 var timeLeft = 60;
 var questionsEl = document.getElementById("questions");
+var startBtn = document.getElementById("start-button");
+var choices = document.getElementById("choices")
 // WHEN I click the start button the timer starts
-startEl.addEventListener('click', function() {  
-});
+// startEl.addEventListener('click', function() {  
+// });
 
 function startQuiz() {
-    var startEl = document.querySelector("#start-button");
+    var startEl = document.querySelector("#start-screen");
+    //hide the start screen
+    startEl.setAttribute("class","hide");
+    //show questions
+    questionsEl.removeAttribute("class");
     //start timer
-
+    // function startTimer
     //show question & answers (see ReadME)-- loop for continuing to ask questions?
 
     //correct answer-earn points
@@ -16,15 +23,30 @@ function startQuiz() {
     //wrong answer- lose time
 
     //end quiz when time is up
-
+displayQuestions();
     
 }
 
-//allow user to enter initials and save score
-function saveScore() {
-
+function displayQuestions () {
+    var currentQuestion = questions[questionIndex];
+    currentQuestion.choices.forEach(function(i, choice){
+    var choiceAnswers = document.createElement("button")
+    choiceAnswers.setAttribute("class", "choice")
+    choiceAnswers.setAttribute("value", choice)
+    choiceAnswers.textContent = i + 1 + choice
+    choices.appendChild(choiceAnswers)
+});
 }
 
-localStorage.setItem('saveScore', JSON.stringify(winnerObj));
 
-//play again needed?
+
+// //allow user to enter initials and save score
+// function saveScore() {
+
+// }
+
+// localStorage.setItem('saveScore', JSON.stringify(winnerObj));
+
+// //play again needed?
+
+startBtn.onclick = startQuiz
