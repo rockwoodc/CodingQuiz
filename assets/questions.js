7056cd138e5371308c3a8d924b49b843d7765516
rockwoodc <rockwoodce@gmail.com>
2022-06-20 17:07:20 -0400

    view questions

--- assets/questions.js
+++ assets/questions.js
@@ -2,6 +2,60 @@ var questions = [
     {
         message:"If you want to make a message appear on the screen in a seperate box, which code would you need?",
         choices: ["window.alert()", "window.prompt()", "stringDataType", "An if/else statement"],
-        correctAnswer:"window.alert()"
+        answer:"window.alert()"
+    },
+
+    {
+        message: "Which of the following means 'or'?",
+        choices: ["||", "&&", "!", "i++"],
+        answer: "|"
+    },
+
+    {
+        message: "Which of the following means 'and'?",
+        choices: ["||", "&&", "!", "i++"],
+        answer:  "&&"
+    },
+
+    {
+        message: "Which of the following means 'not'?",
+        choices: ["||", "&&", "!", "i++"],
+        answer: "!"
+    },
+
+    {
+        message: "If statments read as true.",
+        choices: ["True", "false"],
+        answer: "True"
+    },
+
+    {
+        message: "Which variable can be accessed by the whole application?",
+        choices: ["global" , "local"],
+        answer: "global"
+    },
+
+    {
+        message: "Which of these coding languages allows the user to interact with the page?",
+        choices: ["CSS", "HTML", "JavaScript"],
+        answer: "JavaScript"
+    },
+
+    {
+        message: "Which of the following terms means to draw out a webpage before coding?",
+        choices: ["Camel casing", "Wire Framing", "Reseponsive Design", "Variables"],
+        answer: "Wire Framing"
+    },
+
+    {
+        message: "How can you add user interactivity without using JavaScript?",
+        choices: ["Responsive design", "Call to action", "Pseudo classes", "DevTools"],
+        answer: "Pseudo classes"
+    },
+
+    {
+        message: "What is the meaning of the appreviation MVP?",
+        choices: ["Minimum Viable Product", "Most Valuable Player", "Most Veterans Perform", "Make Video Proof"],
+        answer: "Minimus Viable Product"
     }
 ]
--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
 var questionIndex = 0
-var timerEl = document.getElementById("timer");
-var timeLeft = 60;
+var time = questions.length * 10;
+var timerId;
 var questionsEl = document.getElementById("questions");
 var startBtn = document.getElementById("start-button");
-var choices = document.getElementById("choices")
-// WHEN I click the start button the timer starts
-// startEl.addEventListener('click', function() {  
-// });
+var choices = document.getElementById("choices");
+
+
+
 
 function startQuiz() {
     var startEl = document.querySelector("#start-screen");
@@ -15,8 +15,15 @@ function startQuiz() {
     //show questions
     questionsEl.removeAttribute("class");
     //start timer
-    // function startTimer
-    //show question & answers (see ReadME)-- loop for continuing to ask questions?
+    var sec = 60;
+    var time = setInterval(myTimer, 1000);
+    function myTimer() {
+    document.getElementById('timer').innerHTML = sec + " seconds left";
+    sec--;
+    if (sec == -1) {
+        clearInterval(time);
+    }
+}
 
     //correct answer-earn points
 
@@ -24,6 +31,7 @@ function startQuiz() {
 
     //end quiz when time is up
 displayQuestions();
+
     
 }
 
@@ -33,20 +41,37 @@ function displayQuestions () {
     var choiceAnswers = document.createElement("button")
     choiceAnswers.setAttribute("class", "choice")
     choiceAnswers.setAttribute("value", choice)
+    //allows questions to start at 1 instead of 0
     choiceAnswers.textContent = i + 1 + choice
-    choices.appendChild(choiceAnswers)
-});
-}
+    //allows question to appear
+    var messageEl = document.getElementById("message");
+    messageEl.textContent = currentQuestion.message
+    choices.appendChild(choiceAnswers);
+    });
+} 
 
+
+function answerClick() {
+    //check if answer is wrong
+    if (this.value !== questions[questionIndex].answer) {
+        //lose time
+        time -= 15;
+        
+    //gain points if answer is correct
+
+    //go to next question
+    questionIndex++;  
+    }
+}
 
 
 // //allow user to enter initials and save score
 // function saveScore() {
 
+
 // }
 
 // localStorage.setItem('saveScore', JSON.stringify(winnerObj));
 
-// //play again needed?
-
+//once start button is clicked, the quiz & timer begins
 startBtn.onclick = startQuiz
